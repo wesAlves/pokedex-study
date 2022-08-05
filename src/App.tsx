fa7a4e325377bd6156c96e7e8c6093005ecea64f
Wes <wal2.designer@gmail.com>
2022-08-05 17:42:25 +0100

    add some comments

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,17 @@ interface PokemonData {
 }
 
 const App = () => {
-  const [pokemons, setPokemons] = useState<PokemonData[]>([]);
-  const [count, setCount] = useState(0);
-  const [next, setNext] = useState("");
-  const [previous, setPrevious] = useState("");
+  const [pokemons, setPokemons] = useState<PokemonData[]>([]); //state to hold pokemon array
+  const [count, setCount] = useState(0); //state to hold the total of entried in the response of pokemons
+  const [next, setNext] = useState(""); // state to hold the URL for next pagination
+  const [previous, setPrevious] = useState(""); // state to hold the URL for previous pagination
 
+  //getting pokemons from api
   const getPokemons = async (url: string) => {
     try {
       const response = await pokedex.get(url);
-      console.log(response.data);
+      // console.log(response.data);
+
       setCount(response.data.count);
       setPokemons(response.data.results);
       setNext(response.data.next);
@@ -29,10 +31,6 @@ const App = () => {
   useEffect(() => {
     getPokemons("/pokemon");
   }, []);
-
-  // const handleNext = (next) => {
-  //   getPokemons()
-  // }
 
   return (
     <>
@@ -48,10 +46,17 @@ const App = () => {
           return <PokeCard key={name} name={name} url={url} />;
         })}
       </div>
+      {previous === null ? null : (
+        <button onClick={() => getPokemons(previous)}>Previous</button>
+      )}
 
-      <button onClick={() => getPokemons(previous)}>Previous</button>
-      {Math.floor(count / 20)}
-      <button onClick={() => getPokemons(next)}>Next</button>
+      {Math.floor(count / 20) /*Total of entries*/}
+
+      {/*1...4|5|6...57 pagination should look like this*/}
+
+      {next === null ? null : (
+        <button onClick={() => getPokemons(next)}>Next</button>
+      )}
     </>
   );
 };
