b1142c511ab8870971e9b8d0e92c9e327a14e5f3
Wes <wal2.designer@gmail.com>
2022-08-05 16:27:05 +0100

    getting pokemons and render in the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
+import { useEffect, useState } from "react";
 import { pokedex } from "./api/api";
 
+interface PokemonData {
+  name: string;
+  url: string;
+}
+
 const App = () => {
+  const [pokemons, setPokemons] = useState<PokemonData[]>([]);
+  const [next, setNext] = useState();
+  const [previous, setPrevious] = useState();
+
   const getPokemons = async () => {
     try {
       const response = await pokedex.get("/pokemon");
-      console.log(response);
+      console.log(response.data);
+      setPokemons(response.data.results);
+      setNext(response.data.next);
+      setPrevious(response.data.previous);
     } catch (error) {
       console.error(error);
     }
   };
 
-  return <div onClick={getPokemons}>Pokedex</div>;
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  return (
+    <>
+      {pokemons.map(({ name, url }: PokemonData) => {
+        return (
+          <div key={name}>
+            <h3>{name}</h3>
+            <a href={url}>Details</a>
+          </div>
+        );
+      })}
+      <button>Previous</button>
+      123
+      <button>Next</button>
+    </>
+  );
 };
 
 export default App;
