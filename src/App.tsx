2f774d4c22a19beb4a863cb52bcc5a924bfafee4
Wes <wal2.designer@gmail.com>
2022-08-05 16:06:29 +0100

    getting 20 first pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { pokedex } from "./api/api";
 const App = () => {
   const getPokemons = async () => {
     try {
-      const response = await pokedex.get("/");
+      const response = await pokedex.get("/pokemon");
       console.log(response);
     } catch (error) {
       console.error(error);
