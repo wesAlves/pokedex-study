19a947a189a8c6f1c9c3079cd578390f3a4f82fc
Wes <wal2.designer@gmail.com>
2022-08-06 09:52:49 +0100

    pagination range adjusted

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -13,7 +13,9 @@ export const PokePagination = ({
   active,
   changeFn,
 }: PokePaginationProps) => {
-  const items = [...Array(Math.floor(count / 20)).keys()]; //create an array with given number and populate it with the number / by 20 that is my pagination offset
+  const items = [
+    ...Array(count % 20 > 1 ? Math.floor(count / 20) + 1 : count / 20).keys(),
+  ]; //create an array with given number and populate it with the number / by 20 that is my pagination offset
 
   const paginationStartSlice = active < 2 ? items[0] : active - 1; //set the first position to slice the array of items
   //   const paginationEndSlice = active <= 1 ? active + 6 : active + 4; //set the first position to slice the array of items
