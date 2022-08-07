5325175d20b3264ce438d2aac60a0afea10389cf
Wes <wal2.designer@gmail.com>
2022-08-07 11:11:49 +0100

    test envirionment setttled

--- src/components/pagination/Pagination.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-
-describe("Pagination component", () => {
-  it("Should render when pokemon list > 20", () => {
-    // expect()
-  });
-});
---
+++ src/page/favoriteList/Favorite.spec.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+describe("Favorite functions", () => {
+  it("Should be able to add to favorite", () => {
+    const pokemon = "pica";
+    const { debug } = render(<div>asdf</div>);
+  });
+});
